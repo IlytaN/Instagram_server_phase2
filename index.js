d459945c5afab797eefc5dba2a690b10f576b07e
Ilyta N <snowprincess2305@yahoo.com>
2017-02-16 15:00:23 +0200

    add a bit data

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ var posts = [
             user: {
                 id: 1,
                 username: "dtrump",
-                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg" 
-            },                                                 
+                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+            },
             image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
             imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-            likes: 892, 
+            likes: 892,
             caption: "Always winning #elections",
-            tags: ['elections'],         
+            tags: ['elections'],
             comments: [
                 {
                     id: 0,
                     user: {
                         id: 2,
                         username: "POTUS"
-                    },                    
+                    },
                     comment: "You're never going to make it don #losing",
                     userRefs: [],
                     tags: ["losing"]
@@ -44,10 +44,46 @@ var posts = [
                     user: {
                         id: 3,
                         username: "HillaryC"
-                    },                    
+                    },
                     comment: "Damn right @POTUS",
                     userRefs: ["POTUS"],
-                    tags: []       
+                    tags: []
+                },
+            ]
+
+        },
+        {
+            id: 1,
+            user: {
+                id: 1,
+                username: "dtrump",
+                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+            },
+            image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+            imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+            likes: 892,
+            caption: "test on #elections",
+            tags: ['elections'],
+            comments: [
+                {
+                    id: 0,
+                    user: {
+                        id: 2,
+                        username: "POTUS"
+                    },
+                    comment: "You're never going to make it don #losing",
+                    userRefs: [],
+                    tags: ["losing"]
+                },
+                {
+                    id: 1,
+                    user: {
+                        id: 3,
+                        username: "HillaryC"
+                    },
+                    comment: "Damn right @POTUS",
+                    userRefs: ["POTUS"],
+                    tags: []
                 },
             ]
 
@@ -58,7 +94,7 @@ app.get('/posts/relevant', function(req, res) {
     res.json(posts);
 });
 
-app.get('/posts/:id', function(req, res) {    
+app.get('/posts/:id', function(req, res) {
     res.json(posts[req.params.id]);
 });
 
