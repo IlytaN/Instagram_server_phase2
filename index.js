a25766c23d45a0afa65cdc49f75917b4959aa7b3
Ilyta N <snowprincess2305@yahoo.com>
2017-02-18 14:00:06 +0200

    add login

--- index.js
+++ index.js
@@ -14,7 +14,35 @@ app.use(cors());
 app.get('/', function(req, res) {
     res.send("Hello world");
 });
+// dummy users, in real life these would be in a database or something
+var users = [{
+        id: "txgw35",
+        username: "test",
+        password: "pwd"
+    },
+    {
+        id: "xvj2f2",
+        username: "john",
+        password: "doe"
+    }
+    ];
+    
+app.post('/login', function(req,res){
+    console.log("test");
+    console.log(req.body);
+    var u = users.find(function(element){
+         return (element.username === req.body.username) && (element.password === req.body.password);
+    });
 
+    if(u !== undefined)
+    {
+        return res.json({id: u.id, username: u.username});
+    }
+    else
+    {
+        return res.sendStatus(401);
+    }
+})
 var posts = [
         {
             id: 0,
